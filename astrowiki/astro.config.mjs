d5032ba5ee713e7272a20844cd361128b3cf215f
erixweb <erikmorenodev@gmail.com>
2023-01-28 15:29:28 +0100

    Deploy vercel

--- astrowiki/astro.config.mjs
+++ astrowiki/astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
-    base: '/astrowiki',
     root: "astrowiki",
     publicDir: "public",
 });
