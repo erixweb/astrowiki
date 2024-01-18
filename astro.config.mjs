a62be610593b6203331a3ce8548dc242ec8e321b
erixweb <erikmorenodev@gmail.com>
2024-01-18 14:28:28 +0100

    Fix astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    publicDir: "public",
-    output: 'server',
-    adapter: vercel(),
+  publicDir: "public",
+  output: 'server',
+  adapter: vercel()
 });
