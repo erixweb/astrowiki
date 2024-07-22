0ac1cd1c4371fab00a2892ad9d874e4b73e6cf98
erixweb <erikmorenodev@gmail.com>
2024-07-22 09:37:05 +0200

    Update deps + Start Redesign

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   publicDir: "public",
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [tailwind()]
 });
