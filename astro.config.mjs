38752de23c162c4983d55f2a224f12ea65bde1d1
erixweb <erikmorenodev@gmail.com>
2024-07-23 21:00:35 +0200

    Add robots.txt & astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   publicDir: "public",
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind()]
+  site: "https://astrowiki.vercel.app",
+  integrations: [tailwind(), sitemap()]
 });
